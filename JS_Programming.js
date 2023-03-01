09abc3d99204899f48d072e19856d0b988813974
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-03-01 22:51:22 +0530

    Added To Get Random Number From Dice[1-6]

--- JS_Programming.js
+++ JS_Programming.js
@@ -1,4 +1,12 @@
 //Sequence Practice Problems
-//1- Use REPL – Random Function Math.floor(Math.random() * 10); to get SingleDigit.
-var randomnumber=Math.floor(Math.random()*10);
-console.log(randomnumber);
+
+
+//2- Use REPL – Use Random to get Dice Number between 1 to 6.
+//Function to generate random number from dice roll
+function randomNumber(min, max) 
+{
+    return Math.floor(Math.random() * (max - min) + min);
+}
+  
+console.log("Random Number between 1 and 6: ")
+console.log( randomNumber(1, 6) );
