21d2b2ea8277996b20cfcdf68ef1ed81d63f1e39
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-03-01 22:40:08 +0530

    Added Random Numbers From [0-9] Code

---
+++ JS_Programming.js
@@ -0,0 +1,4 @@
+//Sequence Practice Problems
+//1- Use REPL – Random Function Math.floor(Math.random() * 10); to get SingleDigit.
+var randomnumber=Math.floor(Math.random()*10);
+console.log(randomnumber);
