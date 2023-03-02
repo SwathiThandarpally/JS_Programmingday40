9db838982bf51183eeb7c8e443945933c60afbb2
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-03-02 21:40:48 +0530

    Added If Else Statement Code

---
+++ IfStatement/JS_Programming.js
@@ -0,0 +1,25 @@
+//If Else Statement - Write a program that takes day and month from the command line and prints true if of month is between March 20 and June 20, false otherwise.
+console.log(" Check day of month is between March 20 and June 20");
+ day=(Math.floor(Math.random() * 10)%31)+1;
+ month=(Math.floor(Math.random() * 10)%6)+1;
+console.log("Day"+day+" Month"+month);
+if ( day> 20 && day< 31 && month == 3 )
+{
+console.log("True");
+}
+else if ( day> 1 && day< 28 && month == 1 )
+{
+console.log("True");
+}
+else if ( day> 1 && day< 11 && month == 2 )
+{
+console.log("True");
+}
+else if ( day> 1 && day< 30 && month == 3 )
+{
+console.log("True");
+}
+else
+{
+console.log(" False");
+}
