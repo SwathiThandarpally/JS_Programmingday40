b959220158944c5f8b26c5a64804139459b3315f
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-03-02 23:38:28 +0530

    Added Switch Case Statement Code

---
+++ SwitchCase/SingleNumbers.js
@@ -0,0 +1,40 @@
+//Read a single digit number and write the number in word
+
+let number = ("6");//enter single digit number(0-9)
+
+switch(number)
+{
+   case '0':
+       console.log("zero: Valid Digit");
+   break;
+   case '1' : 
+       console.log("one: Valid Digit");
+   break;
+   case '2' : 
+       console.log("two: Valid Digit");
+   break;
+   case '3' : 
+       console.log("three: Valid Digit");
+   break;
+   case '4':
+       console.log("four: Valid Digit");
+   break;
+   case '5':
+       console.log("five: Valid Digit");
+   break;
+   case '6':
+       console.log("six: Valid Digit");
+   break;
+   case '7':
+       console.log("seven: Valid Digit");
+   break;
+   case '8':
+       console.log("eight: Valid Digit");
+   break;
+   case '9':
+       console.log("nine: Valid Digit");
+   break;
+  default :
+       console.log("Invalid: Not a single digit input");
+  break;
+}
