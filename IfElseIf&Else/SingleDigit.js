046453a4cbb764f73120664005a5ec31746641c4
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-03-02 23:02:07 +0530

    Added If, Else if and Else Statement Code

---
+++ IfElseIf&Else/SingleDigit.js
@@ -0,0 +1,43 @@
+//Read a single digit number and write the number in word
+number = ("1");//enter single dgit number
+
+ if (number == 1)
+ {
+     console.log("one : Valid Number");
+ }
+ else if (number == 2)
+ {
+     console.log("two: Valid Number");
+ }
+ else if (number == 3)
+ {
+     console.log("three: Valid Number");
+ }
+ else if (number == 4)
+ {
+     console.log("four: Valid Number");
+ }
+ else if (number == 5)
+ {
+     console.log("five: Valid Number");
+ }
+ else if (number == 6)
+ {
+     console.log("six: Valid Number");
+ }
+ else if (number == 7)
+ {
+     console.log("seven: Valid Number");
+ }
+ else if (number == 8)
+ {
+     console.log("eight: Valid Number");
+ }
+ else if (number == 9)
+ {
+     console.log("nine: Valid Number");
+ }
+ else
+ {
+     console.log("Invalid input");
+ }
